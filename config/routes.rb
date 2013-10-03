9c2cbac43f3fd314a2378ae860ecbd75a100fe44
Devin <devinwchen@gmail.com>
2013-10-03 00:15:34 -0700

    "added routes"

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ FirstApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'public/index.html' "added public/index/html"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
